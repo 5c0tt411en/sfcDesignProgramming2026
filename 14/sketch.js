21e1e80a1b4f5b11e639323082cea282a50eaefe
5c0tt411en <5c0tt.411en@gmail.com>
2026-06-05 06:36:50 +0900

    added final project

---
+++ 14/sketch.js
@@ -0,0 +1,13 @@
+/*
+1. プログラミングを使用することで，既存のソフトウェアにはない範囲のパラメータを割り当てることでさらに多様な組み合わせをデザインできた．
+2. 使用コード：
+  - https://www.openprocessing.org/sketch/492096 内のAddNewParticle部分（引用元xx - xx行目->実装先xx - xx行目，引用元xx - xx行目->実装先xx - xx行目）
+  - Processingサンプル，ArrayObject内，Moduleクラス部分（引用元xx - xx行目->実装先xx - xx行目，引用元xx - xx行目->実装先xx - xx行目）
+  などのようにわかるようになっていれば可
+*/
+
+function setup() {
+}
+
+function draw() {
+}
